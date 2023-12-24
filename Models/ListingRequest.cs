991527c5c1926ce6de26bbfd280d3f52cc7d5400
Yaroslav Yakupov <yakupov.yaroslav@icloud.com>
2023-12-24 22:16:13 +0300

    add first model

---
+++ Models/ListingRequest.cs
@@ -0,0 +1,10 @@
+namespace AsyncPlatforms.Models;
+
+public class ListingRequest
+{
+    public int Id { get; set; }
+    public string? RequestBody { get; set; }
+    public string? EstimatedCompetionTime { get; set; }
+    public string? RequestStatus { get; set; }
+    public string RequestId { get; set; }
+}
