9e8d26589a583b7d535e9ff40418cab7f95a50c8
Yaroslav Yakupov <yakupov.yaroslav@icloud.com>
2023-12-25 20:07:00 +0300

    add endpoint

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using AsyncPlatforms.Data;
+using AsyncPlatforms.Models;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,5 +9,18 @@ builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlite("Data Source=Re
 var app = builder.Build();
 
 app.UseHttpsRedirection();
+
+app.MapPost("api/v1/platforms", async (AppDbContext context, ListingRequest listingRequest) =>
+{
+    if (listingRequest == null) return Results.BadRequest();
+
+    listingRequest.RequestStatus = "ACCEPT";
+    listingRequest.EstimatedCompetionTime = "2023-12-26:17:00:00";
+
+    await context.ListingRequests.AddAsync(listingRequest);
+    await context.SaveChangesAsync();
+
+    return Results.Accepted($"api/v1/platformstatus/{listingRequest.RequestId}", listingRequest);
+});
 
 app.Run();
