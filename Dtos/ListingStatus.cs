c152839d0ceaa050c55f381f01469281ebd7e374
Yaroslav Yakupov <yakupov.yaroslav@icloud.com>
2023-12-25 20:32:07 +0300

    add another endpoint

---
+++ Dtos/ListingStatus.cs
@@ -0,0 +1,8 @@
+namespace AsyncPlatforms.Dtos;
+
+public class ListingStatus
+{
+    public string? RequestStatus { get; set; }
+    public string? EstimatedCompetionTime { get; set; }
+    public string? ResourceURL { get; set; }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using AsyncPlatforms.Data;
+using AsyncPlatforms.Dtos;
 using AsyncPlatforms.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -21,6 +22,27 @@ app.MapPost("api/v1/platforms", async (AppDbContext context, ListingRequest list
     await context.SaveChangesAsync();
 
     return Results.Accepted($"api/v1/platformstatus/{listingRequest.RequestId}", listingRequest);
+});
+
+app.MapGet("api/v1/platforms/{requestId}", (AppDbContext context, string requestId) =>
+{
+    var listingRequest = context.ListingRequests.FirstOrDefault(lr => lr.RequestId == requestId);
+    if (listingRequest == null) return Results.NotFound();
+
+    ListingStatus listingStatus = new ListingStatus
+    {
+        RequestStatus = listingRequest.RequestStatus,
+        ResourceURL = String.Empty
+    };
+
+    if (listingRequest.RequestStatus.ToUpper() == "COMPLETE")
+    {
+        listingStatus.ResourceURL = $"api/v1/platforms/{Guid.NewGuid().ToString()}";
+        return Results.Ok(listingStatus);
+    }
+
+    listingStatus.EstimatedCompetionTime = "2023-12-26:19:00:00";
+    return Results.Ok(listingStatus);
 });
 
 app.Run();
